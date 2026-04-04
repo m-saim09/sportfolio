83fab73fb26534c16bafeec03ed02497591a17f8
m-saim09 <codewithsaim01@gmail.com>
2026-04-05 01:51:04 +0500

    Remove admin dashboard and related routes

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import express from "express";
 import dotenv from "dotenv";
 import chatHandler from "./api/chat.js";
 import contactHandler from "./api/contact.js";
-import adminSubmissionsHandler from "./api/admin/submissions.js";
-import adminChatHistoryHandler from "./api/admin/chat-history.js";
 
 dotenv.config();
 
@@ -20,11 +18,6 @@ app.use(express.static(__dirname));
 
 app.post("/api/chat", chatHandler);
 app.post("/api/contact", contactHandler);
-app.get("/api/admin/submissions", adminSubmissionsHandler);
-app.get("/api/admin/chat-history", adminChatHistoryHandler);
-app.get("/dashboard", (_req, res) => {
-  res.sendFile(path.join(__dirname, "dashboard.html"));
-});
 
 app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
